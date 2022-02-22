0706320653096859162ce0cf9ae99daffdf6404b
Adrien <adrien@pop-os.localdomain>
2022-02-22 10:44:08 +0100

    add cpp lsp support
    Author:    Adrien MOREAU <moreauadrien92@gmail.com>

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,16 +1,24 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local function on_attach()
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+    vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
+    vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
+    vim.keymap.set("n", "<leader>td", "<cmd>Telescope diagnostics<cr>", {buffer=0})
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+end
+
 require("lspconfig").gopls.setup{
     capabilities = capabilities,
-    on_attach = function()
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-        vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
-        vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-        vim.keymap.set("n", "<leader>td", "<cmd>Telescope diagnostics<cr>", {buffer=0})
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
-    end,
+    on_attach = on_attach
+}
+
+require("lspconfig").clangd.setup{
+    capabilities = capabilities,
+    on_attach = on_attach
 }
 
 
